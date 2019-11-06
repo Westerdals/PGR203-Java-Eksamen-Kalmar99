4e90cd7cd29064b5cf6c70feae1c21c4cedb55c6
Kristoffer <kristoffer-2000@hotmail.com>
2019-11-06 17:58:37 +0100

    Making database and adding a simple method off adding new members

--- src/test/java/no/kristiania/dbtest/MemberTest.java
+++ src/test/java/no/kristiania/dbtest/MemberTest.java
@@ -1,8 +1,10 @@
 package no.kristiania.dbtest;
 
 import no.kristiania.db.MemberDao;
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,8 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberTest {
 
     @Test
-    void shouldRetrieveStoredMembers() {
-        MemberDao dao = new MemberDao();
+    void shouldRetrieveStoredMembers() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+
+        dataSource.getConnection().createStatement().executeUpdate(
+                "create table members(name varchar(100))"
+        );
+
+        MemberDao dao = new MemberDao(dataSource);
         String memberName = pickOne(new String[] {"Kriss", "Kalmar", "Andre", "Tredje"});
         dao.insertMembers(memberName);
         assertThat(dao.listAll()).contains(memberName);
